746ac3240430312860169e593306798c85f18471
basedgrit <dw.is.life11@gmail.com>
2020-02-16 23:18:58 -0500

    Removed window.onload and changed the way console.log works. Closes #1

--- courier.js
+++ courier.js
@@ -1,12 +1,20 @@
-window.onload = loadDocument;
-
-function loadDocument(){
-  //For now this function will only be used for starting the game when
-  //the page loads, eventually there will be a button to start the game
+(function (){
+  if(!console) {
+    console = {};
+  }
+  var old = console.log;
+  var logger = document.querySelector("#text");
+  console.log = function (message) {
+    if (typeof message == 'object'){
+      logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message): String(message)) + '<br />';
+    } else{
+      logger.innerHTML += message + '<br />';
+    }
+  }
+})();
   startGame();
-};
 
 function startGame(){
-  document.getElementById("text").innerHTML = "You wake up in a daze";
-  document.getElementById("text").innerHTML = "As your vision starts clearing up you see a man sitting directly beside you";
-}
+  console.log('You wake up in a daze');
+  console.log('testing');
+};
